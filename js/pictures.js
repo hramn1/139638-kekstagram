1b7d9d43b111a339191ad4052893a48d72f51c8f
Unknown <stmihailov@yandex.ru>
2018-04-30 20:08:57 +0300

    правки

--- js/pictures.js
+++ js/pictures.js
@@ -3,15 +3,6 @@
   var template = document.querySelector('#picture').content;
   var pictures = document.querySelector('.pictures');
   var bigPicture = document.querySelector('.big-picture');
-  // var comment = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
-  //   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  //   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  //   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  //   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  // ];
-  // var description = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят',
-  //   'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-  //   'Вот это тачка!'];
 
 
   // var photos = showImg();
@@ -23,27 +14,12 @@
     return Math.floor(min + Math.random() * (max + 1 - min));
   }
 
-  // function generatePhoto() {
-  //   photos = [];
-  //   for (var i = 1; i <= 25; i++) {
-  //     var link = 'photos/' + i + '.jpg';
-  //     photos.push(
-  //         {
-  //           url: link,
-  //           likes: generateRandomNumber(15, 200),
-  //           comments: comment[generateRandomNumber(0, 5)] + ' ' + comment[generateRandomNumber(0, 5)],
-  //           description: description[generateRandomNumber(0, 5)]
-  //         }
-  //     );
-  //   }
-  //   return photos;
-  // }
   function showImg(data) {
-
     var photos = data;
     var fragmentImg = document.createDocumentFragment();
     photos.forEach(function (item, i) {
       var templatePhoto = template.cloneNode(true);
+      templatePhoto.querySelector('.picture__link').setAttribute('data-index', i);
       templatePhoto.querySelector('.picture__img').src = data[i].url;
       templatePhoto.querySelector('.picture__stat--likes').textContent = data[i].likes;
       templatePhoto.querySelector('.picture__stat--comments').textContent = data[i].comments.length;
